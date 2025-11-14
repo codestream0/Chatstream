0e020835b465c842dc2c9fb34feb18438d6da155
codestream0 <hamzaabdulfatah235@gmail.com>
2025-11-14 16:59:40 +0100

    refreshToken implemented

---
+++ src/lib/baseQueryWithReauth.ts
@@ -0,0 +1,52 @@
+import type { RootState } from "./store";
+import {  fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3300",
+  credentials: "include",
+  prepareHeaders: (headers = new Headers(), { getState }) => {
+    const token = (getState() as RootState)?.auth?.accessToken;
+    if (token) headers.set("Authorization", `Bearer ${token}`);
+    return headers;
+  },
+})
+
+export const baseQueryWithAuth = async (
+  args: any,
+  api: any,
+  extraOptions: any
+) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.status === 401) {
+    console.log("access token expired, refreshing…");
+
+    const refreshResult = await baseQuery(
+      {
+        url: "/auth/refreshToken",
+        method: "POST",
+      },
+      api,
+      extraOptions
+    );
+
+    const refreshData = refreshResult?.data as
+      | { accessToken?: string }
+      | undefined;
+
+    if (refreshData?.accessToken) {
+      api.dispatch({
+        type: "auth/setAccessToken",
+        payload: refreshData.accessToken,
+      });
+
+      return await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch({ type: "auth/logout" });
+      return refreshResult;
+    }
+  }
+
+  return result;
+};
